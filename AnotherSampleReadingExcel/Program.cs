4b30ddfb8e7984fd4026f758f54545041bea66f9
Ken Venhaus <kvenhaus@OEISTL.COM>
2018-05-15 09:15:58 -0500

    loading

---
+++ AnotherSampleReadingExcel/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AnotherSampleReadingExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+
+  //      public static void getExcelFile()
+        {
+
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+
+            // I assume this is where I would use the open file dialog to let the user choose the input file
+
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\kvenhaus\Documents\Items.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+
+                    // I assume this is where I would write the sql query to get the MBS tables I need and then output the extra columns to the excel file
+                }
+            }
+
+            // I assume this is where I would put the save as dialog 
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            //this just keeps window open so you can view it
+            Console.Write("\n\n" + "Any key to continue");
+            Console.ReadKey();
+        
+        }
+    }
+}
+
